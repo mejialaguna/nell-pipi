00b91e8ebf1f011c36dbc6adebad6c812c1b2936
Jose Mejia <mejialaguna@yahoo.com>
2022-04-04 12:26:16 -0400

    idx

--- pages/login.js
+++ pages/login.js
@@ -1,46 +1,47 @@
 import { useEffect, useState } from "react";
-
+import styles from "../styles/Login.module.css";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
-
+import netflixLogo from "../public/static/netflix.svg";
+import cls from "classnames";
+import Loading from "../components/loading"
+import Link from "next/link";
 import { magic } from "../lib/magic-client";
 
-import styles from "../styles/Login.module.css";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
+const regex =
+  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+const Login = function () {
+  const [loading, setLoading] = useState(false);
+  const [isValid, SetIsValid] = useState(false);
   const [userMsg, setUserMsg] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
+  const [email, setEmail] = useState("");
   const router = useRouter();
 
-  useEffect(() => {
-    const handleComplete = () => {
-      setIsLoading(false);
-    };
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router]);
-
-  const handleOnChangeEmail = (e) => {
-    setUserMsg("");
+  function handleOnChange(e) {
     const email = e.target.value;
+    validateEmail(email);
     setEmail(email);
-  };
+    setUserMsg("");
+  }
 
-  const handleLoginWithEmail = async (e) => {
+  function validateEmail(email) {
+    if (regex.test(email)) {
+      SetIsValid(true);
+    } else {
+      SetIsValid(false);
+    }
+  }
+
+  const handleLogin = async (e) => {
     e.preventDefault();
 
-    if (email) {
+    if (email && isValid) {
       // log in a user by their email
       try {
-        setIsLoading(true);
+        setLoading(true);
 
         const didToken = await magic.auth.loginWithMagicLink({
           email,
@@ -58,57 +59,80 @@ const Login = () => {
           if (loggedInResponse.done) {
             router.push("/");
           } else {
-            setIsLoading(false);
+            setLoading(false);
             setUserMsg("Something went wrong logging in");
           }
         }
       } catch (error) {
         // Handle errors if required!
         console.error("Something went wrong logging in", error);
-        setIsLoading(false);
+        setLoading(false);
       }
     } else {
       // show user message
-      setIsLoading(false);
+      setLoading(false);
       setUserMsg("Enter a valid email address");
     }
   };
+
+  useEffect(() => {
+    function handleComplete() {
+      setLoading(false);
+    }
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Netflix SignIn</title>
+        <title> Netflix SignIn</title>
       </Head>
 
       <header className={styles.header}>
         <div className={styles.headerWrapper}>
-          <a className={styles.logoLink} href="/">
-            <div className={styles.logoWrapper}>
-              <Image
-                src="/static/netflix.svg"
-                alt="Netflix logo"
-                width="128px"
-                height="34px"
-              />
-            </div>
-          </a>
+          <Link href="/">
+            <a className={styles.logoLink}>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src={netflixLogo}
+                  width="128px"
+                  height="34px"
+                  alt="netflix logo"
+                />
+              </div>
+            </a>
+          </Link>
         </div>
       </header>
-
       <main className={styles.main}>
         <div className={styles.mainWrapper}>
-          <h1 className={styles.signinHeader}>Sign In</h1>
-
+          <h1 className={styles.signInHeader}> Sign In</h1>
           <input
+            onChange={handleOnChange}
+            label="Email Address"
+            name="Email Address"
+            value={email}
+            className={styles.emailInput}
             type="text"
             placeholder="Email address"
-            className={styles.emailInput}
-            onChange={handleOnChangeEmail}
           />
-
-          <p className={styles.userMsg}>{userMsg}</p>
-          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
-            {isLoading ? "Loading..." : "Sign In"}
-          </button>
+          <p className={styles.userMsg}> {userMsg}</p>
+          {loading ? (
+            <Loading />
+          ) : (
+            <button
+              onClick={handleLogin}
+              className={cls(styles.loginBtn, !email && styles.disabled)}
+              disabled={!email}
+            >
+              Sign In
+            </button>
+          )}
         </div>
       </main>
     </div>
