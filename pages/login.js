c8cd3237786fcdf60d821cbd34eda37f8125bd58
Jose Mejia <mejialaguna@yahoo.com>
2022-04-04 16:18:23 -0400

    login

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
-
+import cls from "classnames";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
-
+import Loading from "../components/loading"
 import { magic } from "../lib/magic-client";
 import styles from "../styles/Login.module.css";
-
+import Link from "next/link";
+import netflixLogo from "../public/static/netflix.svg"
 var validator = require("email-validator");
 
 const Login = () => {
@@ -89,39 +90,49 @@ const Login = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Netflix SignIn</title>
+        <title> Netflix SignIn</title>
       </Head>
 
       <header className={styles.header}>
         <div className={styles.headerWrapper}>
-          <a className={styles.logoLink} href="/">
-            <div className={styles.logoWrapper}>
-              <Image
-                src="/static/netflix.svg"
-                alt="Netflix logo"
-                width="128px"
-                height="34px"
-              />
-            </div>
-          </a>
+          <Link href="/">
+            <a className={styles.logoLink}>
+              <div className={styles.logoWrapper}>
+                <Image
+                  src={netflixLogo}
+                  width="128px"
+                  height="34px"
+                  alt="netflix logo"
+                />
+              </div>
+            </a>
+          </Link>
         </div>
       </header>
-
       <main className={styles.main}>
         <div className={styles.mainWrapper}>
-          <h1 className={styles.signinHeader}>Sign In</h1>
-
+          <h1 className={styles.signInHeader}> Sign In</h1>
           <input
+            onChange={handleOnChangeEmail}
+            label="Email Address"
+            name="Email Address"
+            value={email}
+            className={styles.emailInput}
             type="text"
             placeholder="Email address"
-            className={styles.emailInput}
-            onChange={handleOnChangeEmail}
           />
-
-          <p className={styles.userMsg}>{userMsg}</p>
-          <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
-            {isLoading ? "Loading..." : "Sign In"}
-          </button>
+          <p className={styles.userMsg}> {userMsg}</p>
+          {isLoading ? (
+            <Loading />
+          ) : (
+            <button
+              onClick={handleLoginWithEmail}
+              className={cls(styles.loginBtn, !email && styles.disabled)}
+              disabled={!email}
+            >
+              Sign In
+            </button>
+          )}
         </div>
       </main>
     </div>
