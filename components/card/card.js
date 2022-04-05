18cf126c2e85fde41f7836864f5245fc539a0322
Jose Mejia <mejialaguna@yahoo.com>
2022-04-04 20:46:04 -0400

    populat

--- components/card/card.js
+++ components/card/card.js
@@ -8,13 +8,13 @@ import styles from "./card.module.css";
 
 const Card = (props) => {
   const {
-    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
+    imgUrl = [] ,
     size = "medium",
     id,
     shouldScale = true,
   } = props;
 
-  const [imgSrc, setImgSrc] = useState(imgUrl);
+  const [backupImgSrc, setBackUpImgSrc] = useState();
   const classMap = {
     large: styles.lgItem,
     medium: styles.mdItem,
@@ -22,7 +22,7 @@ const Card = (props) => {
   };
 
   const handleOnError = () => {
-    setImgSrc(
+    setBackUpImgSrc(
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
     );
   };
@@ -40,7 +40,7 @@ const Card = (props) => {
         {...shouldHover}
       >
         <Image
-          src={imgSrc}
+          src={imgUrl ? imgUrl : backupImgSrc}
           alt="image"
           layout="fill"
           onError={handleOnError}
